a8909b5f49277612522e56d3c8c13e6eec722372
AndriiBa1919 <159924402+AndriiBa1919@users.noreply.github.com>
2024-05-09 02:49:33 +0300

    fin

--- js/task-4.js
+++
@@ -1,9 +0,0 @@
-function findNumber(start, end, divisor) {
-  let number
-  for (number = start; number <= end; number++) {
-    if (number % divisor === 0) {
-      return number;
-    }
-  }
-}
-console.log(findNumber(8, 17, 3));
